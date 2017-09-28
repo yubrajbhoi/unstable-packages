e27cb407cc65d368eca36a6c00f8ef58c2de7c07
its-pointless <bored@goat.si>
2017-09-28 12:13:56 +1000

    add libsigsegv

---
+++ packages/libsigsegv/build.sh
@@ -0,0 +1,5 @@
+TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libsigsegv/
+TERMUX_PKG_DESCRIPTION="GNU libsigsegv is a library for handling page faults in user mode"
+TERMUX_PKG_VERSION=2.11
+TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libsigsegv/libsigsegv-2.11.tar.gz
+TERMUX_PKG_SHA256=dd7c2eb2ef6c47189406d562c1dc0f96f2fc808036834d596075d58377e37a18
