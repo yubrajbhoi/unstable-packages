4040bfdcafe7f79e11ca13bce1cb6d845f0c1987
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-28 14:38:48 +0200

    clamav: reformat patches & build.sh

--- packages/clamav/build.sh
+++ packages/clamav/build.sh
@@ -1,5 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.clamav.net/
 TERMUX_PKG_DESCRIPTION="Anti-virus toolkit for Unix"
+TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_VERSION=0.100.2
 TERMUX_PKG_SRCURL=https://www.clamav.net/downloads/production/clamav-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=4a2e4f0cd41e62adb5a713b4a1857c49145cd09a69957e6d946ecad575206dd6
@@ -23,15 +24,15 @@ share/man/man8/clamav-milter.8
 "
 
 termux_step_pre_configure() {
-    export LIBS="-llog"
+	export LIBS="-llog"
 }
 
 termux_step_post_make_install() {
-    install -Dm600 "${TERMUX_PKG_BUILDER_DIR}/freshclam.conf" \
-        "${TERMUX_PREFIX}/etc/clamav/freshclam.conf"
+	install -Dm600 "${TERMUX_PKG_BUILDER_DIR}/freshclam.conf" \
+		"${TERMUX_PREFIX}/etc/clamav/freshclam.conf"
 }
 
 termux_step_post_massage() {
-    mkdir -p "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/var/lib/clamav"
-    mkdir -p "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/var/log/clamav"
+	mkdir -p "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/var/lib/clamav"
+	mkdir -p "${TERMUX_PKG_MASSAGEDIR}/${TERMUX_PREFIX}/var/log/clamav"
 }
