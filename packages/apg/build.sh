19be80c9b263a399204c93df803784eddf740109
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-28 21:16:19 +0200

    new package: apg
    Warning: project seems dead and source available only
    from Internet Archive.

---
+++ packages/apg/build.sh
@@ -0,0 +1,11 @@
+## Note: APG project seems dead. Official homepage & src urls
+## disappeared.
+TERMUX_PKG_HOMEPAGE=http://www.adel.nursat.kz/apg/index.shtml
+TERMUX_PKG_DESCRIPTION="Automated Password Generator."
+TERMUX_PKG_LICENSE="BSD"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=2.2.3
+TERMUX_PKG_SRCURL=https://web.archive.org/web/20070609184004if_/http://www.adel.nursat.kz/apg/download/apg-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=69c9facde63958ad0a7630055f34d753901733d55ee759d08845a4eda2ba7dba
+TERMUX_PKG_DEPENDS="libcrypt"
+TERMUX_PKG_BUILD_IN_SRC=yes
