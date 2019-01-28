f1c189b330964e92f7f866c77da359e9f60d4690
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-28 14:52:37 +0200

    libmicrohttpd: specify license

--- packages/libmicrohttpd/build.sh
+++ packages/libmicrohttpd/build.sh
@@ -1,5 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://www.gnu.org/software/libmicrohttpd/
 TERMUX_PKG_DESCRIPTION="A small C library that is supposed to make it easy to run an HTTP server as part of another application"
+TERMUX_PKG_LICENSE="LGPL-2.0"
 TERMUX_PKG_VERSION=0.9.62
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=bd3e097d703f5091a6a01b56c0464a90fdd17f5d50478cea50a346b25c88db49
