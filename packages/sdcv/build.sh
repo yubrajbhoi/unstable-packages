94599ce1ea573e4178dc51eddee7be7117bedb9e
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-28 14:58:35 +0200

    sdcv: specify license

--- packages/sdcv/build.sh
+++ packages/sdcv/build.sh
@@ -1,5 +1,6 @@
 TERMUX_PKG_HOMEPAGE=http://dushistov.github.io/sdcv/
 TERMUX_PKG_DESCRIPTION="StarDict Console Version"
+TERMUX_PKG_LICENSES="GPL-2.0"
 TERMUX_PKG_VERSION=0.5.2
 TERMUX_PKG_SRCURL=https://github.com/Dushistov/sdcv/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=ec08dc084a0748d5a89df01a20bbf294f09b0366295cd418000f15d2e043c4f7
