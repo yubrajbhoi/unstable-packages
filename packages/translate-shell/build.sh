38eeefe14ecb77899de358e5e4651478866c0da0
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-28 15:05:05 +0200

    translate-shell: specify license

--- packages/translate-shell/build.sh
+++ packages/translate-shell/build.sh
@@ -1,5 +1,6 @@
 TERMUX_PKG_HOMEPAGE=https://www.soimort.org/translate-shell
 TERMUX_PKG_DESCRIPTION="Command-line translator using Google Translate, Bing Translator, Yandex.Translate, etc."
+TERMUX_PKG_LICENSE="Public Domain"
 TERMUX_PKG_VERSION=0.9.6.9
 TERMUX_PKG_SRCURL=https://github.com/soimort/translate-shell/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=05705c541a5d3c34e0df954a799371a0466c85de26400e632672e073767051b4
