94bfe5f820fdcf00dd0b8b38ae43988f2b5454b9
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-30 03:40:31 +0200

    gopass: depends on git and gnupg

--- packages/gopass/build.sh
+++ packages/gopass/build.sh
@@ -3,8 +3,10 @@ TERMUX_PKG_DESCRIPTION="The slightly more awesome standard unix password manager
 TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=1.8.4
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/gopasspw/gopass/archive/v$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=314b317c49ee7f8c59758ab82cf5f6942ce9179c97e53f0f952741dbd0e5471a
+TERMUX_PKG_DEPENDS="git, gnupg"
 
 termux_step_make() {
 	termux_setup_golang
