9b4f5857681dd77aebb2c0c2206c485fff6c88ff
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-01-30 23:35:17 +0200

    libsigsegv: set maintainer & license

--- packages/libsigsegv/build.sh
+++ packages/libsigsegv/build.sh
@@ -1,5 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://www.gnu.org/software/libsigsegv/
 TERMUX_PKG_DESCRIPTION="GNU libsigsegv is a library for handling page faults in user mode"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
 TERMUX_PKG_VERSION=2.12
 TERMUX_PKG_SRCURL=https://ftp.gnu.org/gnu/libsigsegv/libsigsegv-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=3ae1af359eebaa4ffc5896a1aee3568c052c99879316a1ab57f8fe1789c390b6
