262d623499912468e610c030d19ba485dad95c32
Leonid Plyushch <leonid.plyushch@gmail.com>
2019-02-01 00:23:06 +0200

    new package: cabextract

---
+++ packages/cabextract/build.sh
@@ -0,0 +1,7 @@
+TERMUX_PKG_HOMEPAGE=http://www.cabextract.org.uk/
+TERMUX_PKG_DESCRIPTION="A program to extract Microsoft cabinet (.CAB) files"
+TERMUX_PKG_LICENSE="GPL-2.0"
+TERMUX_PKG_MAINTAINER="Leonid Plyushch <leonid.plyushch@gmail.com> @xeffyr"
+TERMUX_PKG_VERSION=1.9
+TERMUX_PKG_SRCURL=https://cabextract.org.uk/cabextract-$TERMUX_PKG_VERSION.tar.gz
+TERMUX_PKG_SHA256=1bbc793d83c73288acd7e28ce33ec04955a76c73bf6471424ff835d725fcc4c1
